7c441f469a1927c7d514e7e63c1c45d0ace7e1a2
AdonisCaballero <adonisjaviercaballeroalamo@alumno.ieselrincon.es>
2026-05-21 08:57:37 +0100

    Add files via upload

---
+++ LibroDeRecetas/LibroDeRecetas/src/main/java/com/example/LibroDeRecetas/LibroDeRecetasApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibroDeRecetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroDeRecetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibroDeRecetasApplication.class, args);
+	}
+
+}
